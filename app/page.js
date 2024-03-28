a4f2b89dd42d297394e4e4d4126cc0c11992066f
Luke Allen <apl1@unc.edu>
2024-03-28 13:53:46 -0400

    hw 3

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import {Hello} from "@/components/hello";
 export default function Home() {
   return (
     <main>
+      <h1>This website is a work in progress &#9786;</h1>
      <h1>This is the main page.</h1>
     </main>
   );
